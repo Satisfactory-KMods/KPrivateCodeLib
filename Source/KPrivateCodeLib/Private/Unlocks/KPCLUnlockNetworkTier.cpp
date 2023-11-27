0acdfffe5a232202618f2e2b9c6ecf24fd77d3d3
Kyrium Kyri123 <Kyrium olli143@live.de>
2023-11-27 15:13:19 +0100

    fix: any bugs

--- Source/KPrivateCodeLib/Private/Unlocks/KPCLUnlockNetworkTier.cpp
+++ Source/KPrivateCodeLib/Private/Unlocks/KPCLUnlockNetworkTier.cpp
@@ -7,42 +7,42 @@
 #include "FGUnlockSubsystem.h"
 #include "Subsystem/KPCLUnlockSubsystem.h"
 
-void UKPCLUnlockNetworkTier::Unlock(AFGUnlockSubsystem* unlockSubssytem) {
-	Super::Unlock(unlockSubssytem);
-	SendToSubsystem(unlockSubssytem);
+void UKPCLUnlockNetworkTier::Unlock( AFGUnlockSubsystem* unlockSubssytem ) {
+	Super::Unlock( unlockSubssytem );
+	SendToSubsystem( unlockSubssytem );
 }
 
-void UKPCLUnlockNetworkTier::Apply(AFGUnlockSubsystem* unlockSubssytem) {
-	Super::Apply(unlockSubssytem);
-	SendToSubsystem(unlockSubssytem);
+void UKPCLUnlockNetworkTier::Apply( AFGUnlockSubsystem* unlockSubssytem ) {
+	Super::Apply( unlockSubssytem );
+	SendToSubsystem( unlockSubssytem );
 }
 
-void UKPCLUnlockNetworkTier::SendToSubsystem(AFGUnlockSubsystem* unlockSubssytem) {
-	if(unlockSubssytem && ensure(mSchematic)) {
-		AKPCLUnlockSubsystem* Subsystem = AKPCLUnlockSubsystem::Get(unlockSubssytem);
-		check(Subsystem);
-		Subsystem->UnlockNetworkTier(mSchematic);
+void UKPCLUnlockNetworkTier::SendToSubsystem( AFGUnlockSubsystem* unlockSubssytem ) {
+	if( unlockSubssytem && ensure( mSchematic ) ) {
+		AKPCLUnlockSubsystem* Subsystem = AKPCLUnlockSubsystem::Get( unlockSubssytem->GetWorld( ) );
+		fgcheck( Subsystem );
+		Subsystem->UnlockNetworkTier( mSchematic );
 	}
 }
 
-void UKPCLUnlockDecoration::Unlock(AFGUnlockSubsystem* unlockSubssytem) {
-	Super::Unlock(unlockSubssytem);
-	SendToSubsystem(unlockSubssytem);
+void UKPCLUnlockDecoration::Unlock( AFGUnlockSubsystem* unlockSubssytem ) {
+	Super::Unlock( unlockSubssytem );
+	SendToSubsystem( unlockSubssytem );
 }
 
-void UKPCLUnlockDecoration::Apply(AFGUnlockSubsystem* unlockSubssytem) {
-	Super::Apply(unlockSubssytem);
-	SendToSubsystem(unlockSubssytem);
+void UKPCLUnlockDecoration::Apply( AFGUnlockSubsystem* unlockSubssytem ) {
+	Super::Apply( unlockSubssytem );
+	SendToSubsystem( unlockSubssytem );
 }
 
-bool UKPCLUnlockDecoration::IsRepeatPurchasesAllowed_Implementation() const {
+bool UKPCLUnlockDecoration::IsRepeatPurchasesAllowed_Implementation( ) const {
 	return true;
 }
 
-void UKPCLUnlockDecoration::SendToSubsystem(AFGUnlockSubsystem* unlockSubssytem) {
-	if(unlockSubssytem && ensure(mDecorations.Num() > 0)) {
-		AKPCLUnlockSubsystem* Subsystem = AKPCLUnlockSubsystem::Get(unlockSubssytem);
-		check(Subsystem);
-		Subsystem->UnlockDecorations(mDecorations);
+void UKPCLUnlockDecoration::SendToSubsystem( AFGUnlockSubsystem* unlockSubssytem ) {
+	if( unlockSubssytem && ensure( mDecorations.Num() > 0 ) ) {
+		AKPCLUnlockSubsystem* Subsystem = AKPCLUnlockSubsystem::Get( unlockSubssytem->GetWorld( ) );
+		check( Subsystem );
+		Subsystem->UnlockDecorations( mDecorations );
 	}
 }
--- Source/KPrivateCodeLib/Public/Buildable/Modular/KPCLModularExtractorBase.h
+++ Source/KPrivateCodeLib/Public/Buildable/Modular/KPCLModularExtractorBase.h
@@ -16,132 +16,155 @@
 /**
  * 
  */
-UCLASS(Abstract)
-class KPRIVATECODELIB_API AKPCLModularExtractorBase: public AKPCLExtractorBase, public IKPCLModularBuildingInterface {
-	GENERATED_BODY()
+UCLASS( Abstract )
+class KPRIVATECODELIB_API AKPCLModularExtractorBase : public AKPCLExtractorBase, public IKPCLModularBuildingInterface {
+	GENERATED_BODY( )
 
 	public:
-		AKPCLModularExtractorBase();
+		AKPCLModularExtractorBase( );
 
 		// Begin IFGUseableInterface
-		virtual FText GetLookAtDecription_Implementation(class AFGCharacterPlayer* byCharacter, const FUseState& state) const override;
-		virtual bool  IsUseable_Implementation() const override;
-		virtual void  OnUseStop_Implementation(AFGCharacterPlayer* byCharacter, const FUseState& state) override;
-		virtual void  OnUse_Implementation(AFGCharacterPlayer* byCharacter, const FUseState& state) override;
+		virtual FText GetLookAtDecription_Implementation( class AFGCharacterPlayer* byCharacter, const FUseState& state ) const override;
+
+		virtual bool IsUseable_Implementation( ) const override;
+
+		virtual void OnUseStop_Implementation( AFGCharacterPlayer* byCharacter, const FUseState& state ) override;
+
+		virtual void OnUse_Implementation( AFGCharacterPlayer* byCharacter, const FUseState& state ) override;
+
 		// End IFGUseableInterface
 
-		virtual void PreSaveGame_Implementation(int32 saveVersion, int32 gameVersion) override {
-			Super::PreSaveGame_Implementation(saveVersion, gameVersion);
+		virtual void PreSaveGame_Implementation( int32 saveVersion, int32 gameVersion ) override {
+			Super::PreSaveGame_Implementation( saveVersion, gameVersion );
 			mCanHaveBuildabled = true;
 		};
 
-		virtual void OnBuildEffectFinished() override {
-			Super::OnBuildEffectFinished();
+		virtual void OnBuildEffectFinished( ) override {
+			Super::OnBuildEffectFinished( );
 			mCanHaveBuildabled = true;
 		};
 
-		UPROPERTY(SaveGame)
+		UPROPERTY( SaveGame )
 		bool mCanHaveBuildabled = false;
 
 		// Begin IKPCLModularBuildingInterface
-		FORCEINLINE virtual bool GetCanHaveModules_Implementation() override {
-			return mCanHaveBuildabled && IsValid(mModularHandler);
+		FORCEINLINE virtual bool GetCanHaveModules_Implementation( ) override {
+			return ( mCanHaveBuildabled || !IsPlayingBuildEffect( ) ) && IsValid( mModularHandler );
 		}
 
-		FORCEINLINE virtual AFGBuildable* GetMasterBuilding_Implementation() override {
-			if(mMasterBuilding.IsValid()) {
-				return mMasterBuilding.Get();
+		FORCEINLINE virtual AFGBuildable* GetMasterBuilding_Implementation( ) override {
+			if( mMasterBuilding.IsValid( ) ) {
+				return mMasterBuilding.Get( );
 			}
 			return nullptr;
 		}
 
-		FORCEINLINE virtual FPowerOptions                    GetPowerOptions_Implementation() override { return mPowerOptions; }
-		FORCEINLINE virtual UKPCLModularBuildingHandlerBase* GetModularHandler_Implementation() override {
+		FORCEINLINE virtual FPowerOptions GetPowerOptions_Implementation( ) override {
+			return mPowerOptions;
+		}
+
+		FORCEINLINE virtual UKPCLModularBuildingHandlerBase* GetModularHandler_Implementation( ) override {
 			return mModularHandler;
 		}
 
-		FORCEINLINE virtual void SetMasterBuilding_Implementation(AFGBuildable* Actor) override {
+		FORCEINLINE virtual void SetMasterBuilding_Implementation( AFGBuildable* Actor ) override {
 			mMasterBuilding = Actor;
-			OnMasterBuildingReceived(Actor);
+			OnMasterBuildingReceived( Actor );
 		}
 
-		virtual int32 GetModularIndex_Implementation() override;
-		virtual void  ApplyModularIndex_Implementation(int32 Index) override;
-		virtual void  SetAttachedActor_Implementation(AFGBuildable* Actor) override;
-		virtual void  RemoveAttachedActor_Implementation(AFGBuildable* Actor) override;
-		virtual bool  AttachedActor_Implementation(AFGBuildable* Actor, TSubclassOf<UKPCLModularAttachmentDescriptor> Attachment, FTransform Location, float Distance) override;
-		virtual void  OnModulesUpdated_Implementation() override;
+		virtual int32 GetModularIndex_Implementation( ) override;
 
-		virtual void ReadyForVisuelUpdate() override;
+		virtual void ApplyModularIndex_Implementation( int32 Index ) override;
+
+		virtual void SetAttachedActor_Implementation( AFGBuildable* Actor ) override;
+
+		virtual void RemoveAttachedActor_Implementation( AFGBuildable* Actor ) override;
+
+		virtual bool AttachedActor_Implementation( AFGBuildable* Actor, TSubclassOf< UKPCLModularAttachmentDescriptor > Attachment, FTransform Location, float Distance ) override;
+
+		virtual void OnModulesUpdated_Implementation( ) override;
+
+		virtual void ReadyForVisuelUpdate( ) override;
+
 		// End IKPCLModularBuildingInterface
 
 		/** ----- Overwrite ----- */
 		// Begin AActor
-		virtual void GetChildDismantleActors_Implementation(TArray<AActor*>& out_ChildDismantleActors) const override;
-		virtual void PostInitializeComponents() override;
-		virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+		virtual void GetChildDismantleActors_Implementation( TArray< AActor* >& out_ChildDismantleActors ) const override;
+
+		virtual void PostInitializeComponents( ) override;
+
+		virtual void GetLifetimeReplicatedProps( TArray< FLifetimeProperty >& OutLifetimeProps ) const override;
 
 		/** Gather Components directly after Initialize */
-		virtual void BeginPlay() override;
-		virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+		virtual void BeginPlay( ) override;
+
+		virtual void EndPlay( const EEndPlayReason::Type EndPlayReason ) override;
+
 		// End AActor
 
 		/** ------ Events ------ */
 		/** Called on Set new Actor to Buildable */
-		UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Km")
-		void                     OnMasterBuildingReceived(AActor* Actor);
-		FORCEINLINE virtual void OnMasterBuildingReceived_Implementation(AActor* Actor) {
-		}
+		UFUNCTION( BlueprintCallable, BlueprintNativeEvent, Category="Km" )
+		void OnMasterBuildingReceived( AActor* Actor );
+
+		FORCEINLINE virtual void OnMasterBuildingReceived_Implementation( AActor* Actor ) { }
 
 		/** Called on Add new Actor to Buildable */
-		UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Modular Building Events")
-		void OnModulesWasUpdated();
+		UFUNCTION( BlueprintCallable, BlueprintNativeEvent, Category="Modular Building Events" )
+		void OnModulesWasUpdated( );
 
-		UPROPERTY(BlueprintAssignable, Category = "Modular Building Events")
+		UPROPERTY( BlueprintAssignable, Category = "Modular Building Events" )
 		FOnModulesUpdated OnModulesUpdated;
 
 		/** Trigger after a Module was recieved or removed */
-		virtual void OnMeshUpdate() {
-			Event_OnMeshUpdate();
+		virtual void OnMeshUpdate( ) {
+			Event_OnMeshUpdate( );
 		};
 
 		/** Trigger after a Module was recieved or removed */
-		UFUNCTION(BlueprintImplementableEvent)
-		void Event_OnMeshUpdate();
+		UFUNCTION( BlueprintImplementableEvent )
+		void Event_OnMeshUpdate( );
 
 		/** ------ Modular Building ------ */
-		UPROPERTY(BlueprintReadOnly, Replicated, Category="KMods|Modular Building")
-		TWeakObjectPtr<AFGBuildable> mMasterBuilding;
+		UPROPERTY( BlueprintReadOnly, Replicated, Category="KMods|Modular Building" )
+		TWeakObjectPtr< AFGBuildable > mMasterBuilding;
 
-		UPROPERTY(BlueprintReadOnly, Replicated, Category="KMods|Modular Building")
+		UPROPERTY( BlueprintReadOnly, Replicated, Category="KMods|Modular Building" )
 		UKPCLModularBuildingHandlerBase* mModularHandler;
 
-		UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="KMods|Modular Building")
+		UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, Category="KMods|Modular Building" )
 		bool mShouldUseUiFromMaster = false;
 
 		/** Helper Function to get Handler casted */
-		UFUNCTION(BlueprintPure, BlueprintCallable, Category="KMods|Modular Building", meta=(BlueprintAutocast))
-		UKPCLModularBuildingHandlerBase* GetHandler() const { return mModularHandler; }
+		UFUNCTION( BlueprintPure, BlueprintCallable, Category="KMods|Modular Building", meta=(BlueprintAutocast) )
+		UKPCLModularBuildingHandlerBase* GetHandler( ) const {
+			return mModularHandler;
+		}
 
-		template<class T>
-		FORCEINLINE T* GetHandler() const { return Cast<T>(GetHandler()); }
+		template< class T >
+		FORCEINLINE T* GetHandler( ) const {
+			return Cast< T >( GetHandler( ) );
+		}
 
 		/** Helper Function to get Master Building casted */
-		UFUNCTION(BlueprintPure, BlueprintCallable, Category="KMods|Modular Building")
-		AFGBuildable* GetMasterBuildable() const {
-			if(mMasterBuilding.IsValid()) {
-				return mMasterBuilding.Get();
+		UFUNCTION( BlueprintPure, BlueprintCallable, Category="KMods|Modular Building" )
+		AFGBuildable* GetMasterBuildable( ) const {
+			if( mMasterBuilding.IsValid( ) ) {
+				return mMasterBuilding.Get( );
 			}
 			return nullptr;
 		}
 
-		template<class T>
-		FORCEINLINE T* GetMasterBuildable() const { return Cast<T>(GetMasterBuildable()); }
+		template< class T >
+		FORCEINLINE T* GetMasterBuildable( ) const {
+			return Cast< T >( GetMasterBuildable( ) );
+		}
 
 		/** ----- Components ----- */
-		UPROPERTY(SaveGame, Replicated)
+		UPROPERTY( SaveGame, Replicated )
 		int32 mModularIndex = -1;
 
-		UPROPERTY(EditDefaultsOnly, Category="KMods|Debug")
+		UPROPERTY( EditDefaultsOnly, Category="KMods|Debug" )
 		bool mDismantleAllChilds = false;
 };
