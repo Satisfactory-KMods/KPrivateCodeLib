7bd881217c6e774480f06d90ef5724a27b0da191
Kaufmann, Oliver (SI E SCM LPZ MF) <kaufmann.oliver@siemens.com>
2024-09-27 16:16:05 +0200

    chore: remove canceled code

--- Source/KPrivateCodeLib/Private/Interfaces/KPCLDecorInterface.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Copyright Coffee Stain Studios. All Rights Reserved.
-
-
-#include "Interfaces/KPCLDecorInterface.h"
-
-
-// Add default functionality here for any IKPCLDecorInterface functions that are not pure virtual.
